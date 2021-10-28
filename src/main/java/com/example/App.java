bb7afda1c11e94b43f3d4ab67ae071db8fdaa5f7
Guru Dewan <gauravdewan@deloitte.co.uk>
2021-10-28 12:03:27 +0100

    replaced wiremock response definition builder api with json api

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -7,28 +7,33 @@ import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMoc
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static Stubs stubs = new Stubs();
 
-        WireMockServer wireMockServer = new WireMockServer(wireMockConfig().port(8080)); // No-args constructor will
-                                                                                         // start on port 8080, no HTTPS
-        wireMockServer.start();
-        // stub in code, then stub in json files
-        
-        // TODO replace this with json file
-        
-        // mock response
-        ResponseDefinitionBuilder respDefBuilder = new ResponseDefinitionBuilder();
-        respDefBuilder.withStatus(200);
-        //respDefBuilder.withStatusMessage("hi world");
-        respDefBuilder.withBody("hi this is the body");
-        
-        // mock request
-        WireMock.stubFor(WireMock.get("/api/users").willReturn(respDefBuilder));
-        //WireMock.stubFor(WireMock.get("/api/admins").willReturn(respDefBuilder2));
-        
+    public static void main(String[] args) {
+        System.out.println("Setting up the stubs...");
+
+        stubs.setUp().stubForAddProduct("AddProductRequest.json", "AddProductResponse.json")
+                .stubForPutProduct("PutProductRequest.json", "PutProductResponse.json").stubForDeleteProduct().status();
     }
+
+    /*
+     * 
+     * WireMockServer wireMockServer = new
+     * WireMockServer(wireMockConfig().port(8080)); // No-args constructor will //
+     * start on port 8080, no HTTPS wireMockServer.start(); // stub in code, then
+     * stub in json files
+     * 
+     * // TODO replace this with json file
+     * 
+     * // mock response ResponseDefinitionBuilder respDefBuilder = new
+     * ResponseDefinitionBuilder(); respDefBuilder.withStatus(200);
+     * //respDefBuilder.withStatusMessage("hi world");
+     * respDefBuilder.withBody("hi this is the body");
+     * 
+     * // mock request
+     * WireMock.stubFor(WireMock.get("/api/users").willReturn(respDefBuilder));
+     * //WireMock.stubFor(WireMock.get("/api/admins").willReturn(respDefBuilder2));
+     * 
+     */
 }
